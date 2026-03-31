9d63e35db5354ea2aacd5558a855b629b024d14a
krista2732 <krista.pietro@gmail.com>
2026-03-31 00:24:26 -0700

    Created .venv file and added it to .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # eosc-454-project-2026
 My course project on inversion!
+
+This project runs an inversion on resistivity values from a DC survey taken in the Northwest Territories, following a severe forest fire. The surveys were conducted to evaluate the impacts of the forest fire on the permafrost, as the region lies within a discontinuous permafrost zone. 
+
+To run this code, download "inversion.ipynb", "test_project_utils.py" _____.
--- test_project_utils.py
+++ test_project_utils.py
@@ -1,33 +1,39 @@
 import numpy as np
 from project_utils import kernel_function
 
-# test length of kernel function
-def test_length_kernels():
-    n_nodes = 100
-    x_nodes = np.linspace(0, 1, n_nodes)
+####### from class as example!!! (get rid of later)
+# # test length of kernel function
+# def test_length_kernels():
+#     n_nodes = 100
+#     x_nodes = np.linspace(0, 1, n_nodes)
 
-    kernel = kernel_function(x_nodes, 1, 1, 1)
-    passed = len(kernel) == n_nodes # T or F
-    print(f"n_nodes: {n_nodes}, len(kernel): {len(kernel)}, passed?: {passed}")
+#     kernel = kernel_function(x_nodes, 1, 1, 1)
+#     passed = len(kernel) == n_nodes # T or F
+#     print(f"n_nodes: {n_nodes}, len(kernel): {len(kernel)}, passed?: {passed}")
 
-    assert(len(kernel) == n_nodes)
+#     assert(len(kernel) == n_nodes)
 
-# test when exponent equals zero
-def test_exponent_zero():
-    n_nodes = 100
-    x_nodes = np.linspace(0, 1, n_nodes)
-    kernel_index = 1
-    exponent = 0
-    oscillation = 2
+# # test when exponent equals zero
+# def test_exponent_zero():
+#     n_nodes = 100
+#     x_nodes = np.linspace(0, 1, n_nodes)
+#     kernel_index = 1
+#     exponent = 0
+#     oscillation = 2
 
-    kernel = kernel_function(x_nodes, kernel_index=kernel_index, exponent=exponent, oscillation=oscillation)
-    test_kernel = np.cos(2*np.pi*kernel_index*oscillation*x_nodes)
+#     kernel = kernel_function(x_nodes, kernel_index=kernel_index, exponent=exponent, oscillation=oscillation)
+#     test_kernel = np.cos(2*np.pi*kernel_index*oscillation*x_nodes)
 
-    # must account for numerical errors (make threshold)
-    passed = np.allclose(kernel, test_kernel)
+#     # must account for numerical errors (make threshold)
+#     passed = np.allclose(kernel, test_kernel)
 
-    print(f"kernel max: {np.max(kernel)}, test_kernel max: {np.max(test_kernel)}")
-    print(f"kernel min: {np.min(kernel)}, test_kernel min: {np.min(test_kernel)}")
-    print(f"kernel norm: {np.linalg.norm(kernel)}, test_kernel norm: {np.linalg.norm(test_kernel)}")
-    assert(passed)
+#     print(f"kernel max: {np.max(kernel)}, test_kernel max: {np.max(test_kernel)}")
+#     print(f"kernel min: {np.min(kernel)}, test_kernel min: {np.min(test_kernel)}")
+#     print(f"kernel norm: {np.linalg.norm(kernel)}, test_kernel norm: {np.linalg.norm(test_kernel)}")
+#     assert(passed)
 
+#######
+
+# test if apparent resistivities generated from halfspace model is less than 5 % error
+def test_halfspace_app_res_error():
+    
