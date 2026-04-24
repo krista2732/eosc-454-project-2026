55bc02515d20c81663634a0d414b484422b7d451
krista2732 <krista.pietro@gmail.com>
2026-04-24 13:59:27 -0700

    got rid of test file, final updates

--- project_utils.py
+++ project_utils.py
@@ -6,13 +6,19 @@ import ipywidgets
 # file for future functions!
 
 def plot_tikhonov_curves(dictionary, target):
+    '''
+    Plots Tikhonov curves using output dictionary defining results for each iteration.
+    
+    '''
     # initalize arrays
     betas = []
     phi_ms = []
     phi_ds = []
 
     # get list of iterations done
-    list_iterations = dictionary.keys()
+    # sort entries by beta (largest to smallest)
+    list_iterations = sorted(dictionary.keys())
+    print(list_iterations)
 
     for i in list_iterations:
         betas.append(dictionary[i]['beta'])
@@ -21,25 +27,29 @@ def plot_tikhonov_curves(dictionary, target):
 
     target_line = target.target
 
-    fig, ax = plt.subplots(1, 3, figsize=(12, 4)) 
-    
-    ax[0].semilogx(betas, phi_ds)
-    ax[1].semilogx(betas, phi_ms)
-    ax[2].loglog(phi_ms, phi_ds) 
+    fig, ax = plt.subplots(3, 1, figsize=(6, 13)) 
 
     # plot beta at the beta index
-    ax[0].semilogx(betas, phi_ds, "ro")
-    ax[1].semilogx(betas, phi_ms, "ro")
-    ax[2].loglog(phi_ms, phi_ds, "ro")
+    ax[0].loglog(betas, phi_ds, color="blue")
+    ax[0].invert_xaxis() # put betas in descending order
+    ax[1].loglog(betas, phi_ms, color="blue")
+    ax[1].invert_xaxis()
+    ax[2].loglog(phi_ms, phi_ds, color="blue")
     
+    ax[0].set_xlabel(r"$\beta$")
     ax[0].set_ylabel(r"$\phi_d$")
+
+    ax[1].set_xlabel(r"$\beta$")
     ax[1].set_ylabel(r"$\phi_m$")
     
     ax[2].set_xlabel(r"$\phi_m$")
     ax[2].set_ylabel(r"$\phi_d$")
 
     # add target misfit 
-    ax[0].semilogx(betas, np.ones(len(betas))*target_line, "--k")
-    ax[2].loglog(phi_ms, np.ones(len(phi_ms))*target_line, "--k")
+    ax[0].loglog(betas, np.ones(len(betas))*target_line, color="red", linestyle="--", label=fr"Target Misfit: $\phi_d$ = {target_line:.0f}")
+    ax[0].legend()
+    ax[2].loglog(phi_ms, np.ones(len(phi_ms))*target_line, color="red", linestyle="--", label=fr"Target Misfit: $\phi_d$ = {target_line:.0f}")
+    ax[2].legend()
      
     plt.tight_layout()
+    plt.show()
